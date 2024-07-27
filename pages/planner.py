d69a6142e94a2093b6a4ea48d74258c7cb245381
STC1702 <stc17022002@gmail.com>
2024-07-27 15:02:15 +0700

    feat:map planner, map url

--- pages/planner.py
+++ pages/planner.py
@@ -8,8 +8,7 @@ import requests
 import networkx as nx
 import osmnx as ox
 from streamlit_extras.grid import grid
-import pyautogui
-
+from streamlit_star_rating import st_star_rating
 
 import io
 from PIL import Image
@@ -20,24 +19,10 @@ domain = "http://localhost"
 port = ":8080"
 if "final_locations" in st.session_state:
     final_locations = st.session_state["final_locations"]
-# ox.config(use_cache=True, log_console=True)
-# @st.cache_data(show_spinner=False)
-# def fetch_graphml_data(path='graph.graphml'):
-#     return ox.load_graphml(path)
-
-# G = fetch_graphml_data('graph.graphml')
-
-# @st.cache_resource
-# def find_routes(graph, start, end):
-#     return ox.routing.shortest_path(graph, start, end, weight="length")
-
-
-
 # # if 'sidebar_state' not in st.session_state:
 # # st.set_page_config(page_title="Tourism Planner", page_icon="🌍", layout="wide", initial_sidebar_state='collapsed')
 # st.markdown("Tourism Planner")
 # st.sidebar.header("Tourism Planner")
-
 # st.title("Planner")
 
 col1, col2 = st.columns(spec=[4,6])
@@ -45,7 +30,7 @@ col1, col2 = st.columns(spec=[4,6])
 with col2:
     with st.container():
         for i, location in enumerate(final_locations):
-            with st.expander(location["name"]):
+            with st.expander(location["name"], expanded=True):
                 inside_col1, inside_col2 = st.columns(spec=[2,8])
                 with inside_col1:
                     st.image(location["img"])
@@ -54,26 +39,45 @@ with col2:
                     long_text = f"Địa điểm thứ {i+1} trong lịch trình"
                     # https://docs.streamlit.io/develop/api-reference/write-magic/st.write_stream
                     st.write(long_text)
-
 start=[16.4683,107.5786]
 with col1:
-    st_folium(m, width=725, returned_objects=[])
-    # đổi sang GoogleMApEngine
-    exp_btn = st.button("Đồng bộ hóa lịch trình trên google map")
-
-st.write(final_locations)
-
-if exp_btn:
     url = "https://www.google.com/maps/dir"
     for location in final_locations:
-        url += "/" + location["name"] + location["address"]
-    st.write(f'''
-        <a target="_self" href="{url}">
-            <button>
-                Đồng bộ hóa thành công, chuyển đến map
-            </button>
-        </a>
-        ''',
-        unsafe_allow_html=True
-    )    
-    # components.iframe(url+"&output=embed")
+        url += "/" + location["name"] +',' + location["address"]
+    st.link_button("Đồng bộ hóa thành công, chuyển đến map", url)
+    stars = st_star_rating('Bạn đánh giá lộ trình này như thế nào?', 5, 0, size=60, emoticons=True, read_only=False, dark_theme=False, 
+                    #    resetButton=reset_btn, resetLabel=reset_label,
+                    #    customCSS=css_custom, on_click=function_to_run_on_click if enable_on_click else None
+                       )
+    m = folium.Map(location=start, zoom_start=10)
+    gg_res= st.session_state["final_locations"] 
+    if len(gg_res)!=0:
+        place_show = st.session_state["place_show"]
+        for index,place in enumerate(gg_res):
+            if index>0:
+                res_obj = {
+                "origin_node": gg_res[index-1]['node_id'],
+                "destination_node": gg_res[index]['node_id'],
+                }
+                # response = requests.post(url + "/find-route", json = res_obj)
+                response = requests.post(domain + port + "/find-route", json = res_obj)
+                points_list= response.json()
+                folium.PolyLine(locations=points_list, color='blue', dash_array='5, 5',
+                                tooltip=f"From a to b", smooth_factor=0.1,).add_to(m)
+            # Define marker variables
+            name = place['name']
+            website = "#"
+            directions = place['gg_map']
+            pub_html = folium.Html(f"""<p style="text-align: center;"><b><span style="font-family: Didot, serif; font-size: 18px;">{name}</b></span></p>
+            <p style="text-align: center;"><img src="{place['img']}" width="220" height="250">
+            <p style="text-align: center;"><a href={website} target="_blank" title="{name} Website"><span style="font-family: Didot, serif; font-size: 14px;">{name} Website</span></a></p>
+            <p style="text-align: center;"><a href={directions} target="_blank" title="Directions to {name}"><span style="font-family: Didot, serif; font-size: 14px;">Directions to {name}</span></a></p>
+            """, script=True)
+            popup = folium.Popup(pub_html, max_width=220)
+            icon = folium.Icon(color='red', prefix='fa',icon=f'{index+1}')
+            folium.Marker(location=place['coordinate'], tooltip=name, icon=icon, popup = popup).add_to(m)
+
+    folium.FitOverlays().add_to(m)
+    folium.plugins.MiniMap(width=100, height=100).add_to(m)
+    st_data = st_folium(m, width=600, height=400, returned_objects=[])
+
